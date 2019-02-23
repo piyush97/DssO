601fafdb9ee40d26044662ffdbedeed181299e9d
Piyush Mehta <me@piyushmehta.com>
2019-02-23 23:18:41 +0530

    auth

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const authRoutes = require('./routes/auth-routes')
+
 app.set('view engine', 'ejs')
+
+app.use(authRoutes)
 
 app.get('/',(req,res) => {
     res.render('home');
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -3,3 +3,13 @@ const router = require('express').Router()
 router.get('/login', (req, res) => {
     res.render('login')
 })
+
+router.get('/logout', (req,res) => {
+    res.send('logging out')
+})
+
+router.get('/google', (req,res) => {
+    res.send('logging in with google')
+})
+
+module.exports = router
